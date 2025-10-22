124dd2e9706ab098b45c2697da58660e105ac2dd
SHOJOL <shojolislam.webappick@gmail.com>
2025-10-22 13:03:30 +0600

    fix: change default color

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,8 +17,8 @@ interface SmartPlaceholderProps {
  *
  * @param width - Width of the placeholder (default: 300)
  * @param height - Height of the placeholder (default: 200)
- * @param backgroundColor - Base background color (default: '#E1E9EE')
- * @param animationColor - Shimmer/animation color (default: 'rgba(255, 255, 255, 0.4)')
+ * @param backgroundColor - Base background color (default: '#e0e0e0')
+ * @param animationColor - Shimmer/animation color (default : animationStyle === "fade" || animationStyle === "pulse"? "rgba(150, 149, 149, 0.8)" : "rgba(255, 255, 255, 0.8)")
  * @param animationStyle - Type of animation to display (default: 'fade')
  * @param borderRadius - Border radius of the placeholder (default: 4)
  * @param style - Additional custom styles for the container
